99f260a90dfcc119b25c72a5ef3066e0b963ca6c
Krishmalgg <krishmalgamage20021027@gmail.com>
2025-03-08 13:35:18 +0530

    server created

---
+++ server/routes/authRouter.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getAdminDashboard } from "../controllers/authController.js";
+import verifyUser from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.post("/login", verifyUser, getAdminDashboard);
+export default router;
