91b7318a2db1fa18ffb32fe70058e44dd1c2de1c
EshanRavindu17 <eshanravindu17@gmail.com>
2025-03-08 12:06:33 +0530

    Update MyteamView

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Import the router
 
 import PlayersView from './pages/PlayersView';
-import SelectTeamView from './pages/TeamView';
+import MyTeamView from './pages/MyTeamView';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/playersview" element={<PlayersView />} />
-        <Route path="/teamview" element={<SelectTeamView />} />
+        <Route path="/myteamview" element={<MyTeamView />} />
       </Routes>
     </BrowserRouter>
   );
