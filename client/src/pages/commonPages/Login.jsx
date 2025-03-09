3195e8cafcc3bee2488a49ee3f436230ea6959b5
Vidura Kahandawa <viduramalshan185@gmail.com>
2025-03-09 14:20:35 +0530

    routing

--- client/src/pages/commonPages/Login.jsx
+++ client/src/pages/commonPages/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
         localStorage.setItem("name", data.name);
         localStorage.setItem("id",data.id)
   
-        navigate(data.isAdmin ? "/admin-panel" : "/user-dashboard");
+        navigate(data.isAdmin ? "/admin-panel" : "/");
       } else {
         setErrorMessage(data.message);
       }
